9fc62e2f637fba521fc134697f63f18fb5acbb49
pdejoux <philderbeast@gmail.com>
2014-05-08 06:09:30 +1000

    Deedle CSV loading is broken, park it for now.

---
+++ Prototypes/PilotRankFromComps/PilotRankFromComps/Deedle.fsx
@@ -0,0 +1,20 @@
+﻿#I @"../packages/FSharp.Data.2.0.5/lib/net40"
+#I @"../packages/Deedle.0.9.12/lib/net40"
+
+#r "Deedle.dll"
+
+open System
+open System.IO
+open System.Linq
+open Deedle
+
+let root = __SOURCE_DIRECTORY__ + "/../../../Test/"
+let compFile = root + "ForbesPreWorlds.csv"
+
+File.Exists(compFile);;
+let compFrame = Frame.ReadCsv(compFile)
+//SEE: https://github.com/BlueMountainCapital/Deedle/issues/163
+//System.TypeLoadException: Could not load type 'FSharp.Data.Csv.CsvFile' from assembly 'FSharp.Data.DesignTime, Version=2.0.5.0, Culture=neutral, PublicKeyToken=null'.
+//   at Deedle.FrameUtilsModule.readCsv(TextReader reader, FSharpOption`1 hasHeaders, FSharpOption`1 inferTypes, FSharpOption`1 inferRows, FSharpOption`1 schema, String missingValues, FSharpOption`1 separators, FSharpOption`1 culture, FSharpOption`1 maxRows)
+//   at Deedle.FSharpFrameExtensions.Frame.ReadCsv.Static(String path, FSharpOption`1 hasHeaders, FSharpOption`1 inferTypes, FSharpOption`1 inferRows, FSharpOption`1 schema, FSharpOption`1 separators, FSharpOption`1 culture, FSharpOption`1 maxRows) in c:\dev\FSharp.DataFrame\src\Deedle\FrameExtensions.fs:line 268
+//   at <StartupCode$FSI_0005>.$FSI_0005.main@() in C:\dev\Src\hgfa-ranking\Prototypes\PilotRankFromComps\PilotRankFromComps\Deedle.fsx:line 15
--- Prototypes/PilotRankFromComps/PilotRankFromComps/Ranking.fsx
+++ Prototypes/PilotRankFromComps/PilotRankFromComps/Ranking.fsx
@@ -1,8 +1,6 @@
 ﻿#I @"..\packages\FSharp.Data.2.0.7\lib\net40"
-//#I @"..\packages\Deedle.0.9.12"
 
 #r @"FSharp.Data.dll"
-//#load "Deedle.fsx"
 
 open System
 open System.IO
@@ -42,10 +40,6 @@ let groupByScore = query {
         for r in comp.Rows do
         groupBy r.Total
     }
-
-//open Deedle
-//let frameFile = Path.GetFullPath(Path.Combine(__SOURCE_DIRECTORY__, sampleCompFile))
-//let compFrame = Frame.ReadCsv(frameFile, hasHeaders = true, inferTypes = true, inferRows = 0, schema = null, separators = ",", culture = null, maxRows = 0)
 
 type SanctionRanking = AAA | AA | A | B | C
 
