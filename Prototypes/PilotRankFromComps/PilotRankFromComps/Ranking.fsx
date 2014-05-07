62e5dc613c29381b1cb54a04ffcce61f752538d5
pdejoux <philderbeast@gmail.com>
2014-05-07 19:04:45 +1200

    Load Forbes 2012 data from CSV.
    Enable package restore. Update git ignore to ignore packages.

--- Prototypes/PilotRankFromComps/PilotRankFromComps/Ranking.fsx
+++ Prototypes/PilotRankFromComps/PilotRankFromComps/Ranking.fsx
@@ -1,1 +1,9 @@
-﻿
+﻿#r @"..\packages\FSharp.Data.2.0.7\lib\net40\FSharp.Data.dll"
+
+open FSharp.Data
+
+[<Literal>]
+let sampleFile = @"..\..\..\Test\ForbesPreWorlds.csv"
+type CompRank = CsvProvider<sampleFile>
+
+let forbes2012 = CompRank.Load sampleFile
